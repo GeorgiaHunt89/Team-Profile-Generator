0420252ea59e96bb2880d0477627c4c9d0782bce
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-12 13:31:33 +0930

    Constructors added to index

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const inquire = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+
+//Constructors
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
