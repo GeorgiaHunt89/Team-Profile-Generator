a8feff7029cd38acb0dc229f48e7f7d6c6f4b51f
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-14 16:08:24 +0930

    added engineer function to index.js

--- index.js
+++ index.js
@@ -16,9 +16,47 @@ const Intern = require('./lib/Intern');
 const staff = [];
 const idNumber = [];
 
+// Function to record manager answers from user
 inquire.prompt(managerQuest).then((answers) => {
 const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answer.managerOfficeNumber);
 staff.push(manager);
 idNumber.push(answers.managerId);
 addStaff();
-})
+});
+
+// // Function to select next staff role
+// Function addstaff(answers) {
+//     inquire.prompt([
+//     {
+//         type: 'checkbox',
+//         name: 'staffSelection',
+//         message: 'Which staff member would you like to add next?',
+//         choices: [
+//             'Employee', 'Intern', 'Engineer',
+//         ],
+//     },       
+//     ])
+//     .then(userInput => {
+//         switch(userInput.staffSelection){
+//             case 'Engineer':
+//                 addEngineer();
+//                 break;
+//                 case 'Intern':
+//                     addIntern();
+//                     break;
+//                     case 'Employee':
+//                         addEmployee();
+//                         break;
+//                         default:
+//                             generateHTML();
+//         }
+//     });
+// };
+
+// Function to record Engineer answers from user
+inquire.prompt(engineerQuest).then((answers) => {
+    const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answer.engineerGitHub);
+    staff.push(engineer);
+    idNumber.push(answers.engineerId);
+    addStaff();
+    });
