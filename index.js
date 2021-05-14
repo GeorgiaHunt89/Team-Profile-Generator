b62d3455f9d7a980286fca8194f74f524fa7356f
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-14 15:40:50 +0930

    Jest added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquire = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const employeeQuest = require('./quest/Employee.quest');
+const managerQuest = require('./quest/Manager.quest');
+const engineerQuest = require('./quest/Engineer.quest');
+const interQuest = require('./quest/Intern.quest');
 
 //Constructors
 const Employee = require('./lib/Employee');
@@ -10,4 +14,11 @@ const Intern = require('./lib/Intern');
 
 // Empty array to hold staff place holders
 const staff = [];
+const idNumber = [];
 
+inquire.prompt(managerQuest).then((answers) => {
+const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answer.managerOfficeNumber);
+staff.push(manager);
+idNumber.push(answers.managerId);
+addStaff();
+})
