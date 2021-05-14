ca4c12bc9f9045de049059faf53bd268104fdf7f
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-14 15:15:58 +0930

    engineer questions section

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+// Empty array to hold staff place holders
+const staff = [];
+
