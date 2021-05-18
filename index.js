c2d295826401ad947e4783d5c11458dc7e982401
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-18 13:55:52 +0930

    Updated inquirer prompt functions to async

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
 const employeeQuest = require('./quest/Employee.quest');
@@ -16,75 +16,73 @@ const Intern = require('./lib/Intern');
 const staff = [];
 
 // Function to record manager answers from user
-function addManager() {
-inquire.prompt(managerQuest).then((managerAnswers) => {
-const {name, id, email, officeNumber} = managerAnswers;
-const manager = new Manager (name, id, email, officeNumber);
-staff.push(managerAnswers);
-console.log(manager);
-})
+async function addManager() {
+    const managerAnswers = await inquirer.prompt(managerQuest)
+    const {name, id, email, officeNumber} = managerAnswers;
+    const manager = new Manager (name, id, email, officeNumber);
+    staff.push(managerAnswers);
+    console.log(manager)
 };
 
 
 // Function to select next staff role
-const addStaff = () => {
+const addStaff = async () => {
     console.log('Select additional staff members');
 
-   inquirer.prompt([
-    {
-        type: 'checkbox',
-        name: 'staffSelection',
-        message: 'Which staff member would you like to add next?',
-        choices: [
-            'Employee', 'Intern', 'Engineer',
-        ],
-    },       
-    ])
-    .then(userInput => {
-        switch(userInput.staffSelection){
-            case 'Engineer':
-                addEngineer();
-                break;
-                case 'Intern':
-                    addIntern();
-                    break;
-                    case 'Employee':
-                        addEmployee();
-                        break;
-                        default:
-                            generateHTML();
-        }
-        console.log('Add:', userInput.staffSelection);
-    });
+   const userInput = await inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'staffSelection',
+            message: 'Which staff member would you like to add next?',
+            choices: [
+                'Employee', 'Intern', 'Engineer',
+            ],
+        },
+    ]);
+    switch (userInput.staffSelection) {
+        case 'Engineer':
+            addEngineer();
+            break;
+        case 'Intern':
+            addIntern();
+            break;
+        case 'Employee':
+            addEmployee();
+            break;
+        default:
+            generateHTML();
+    }
+    console.log('Add:', userInput.staffSelection);
 };
 
 // Function to record Engineer answers from user
 const addEngineer = async () => {
-    const engineerAnswers = await inquire.prompt(engineerQuest);
+    const engineerAnswers = await inquirer.prompt(engineerQuest);
     const { name, id, email, gitHub } = engineerAnswers;
     const engineer = new Engineer(name, id, email, gitHub);
     staff.push(engineerAnswers);
     console.log(engineer);
+    addStaff();
 };
 
 //Function to record Employee answer from user
-const addEmployee = () => {
-    return inquire.prompt(employeeQuest).then((employeeAnswers) => {
-        const {name, id, email} = employeeAnswers;
-        const employee = new Employee (name, id, email);
-        staff.push(employeeAnswers);
-        console.log(employee);
-        });
+const addEmployee = async () => {
+    const employeeAnswers = await inquirer.prompt(employeeQuest);
+    const { name, id, email } = employeeAnswers;
+    const employee = new Employee(name, id, email);
+    staff.push(employeeAnswers);
+    console.log(employee);
+    addStaff();
 };
 
 //Function to record Intern answer from user
-const addIntern = () => {
-    return inquire.prompt(internQuest).then((interAnswers) => {
-        const {name, id, email, school} = internAnswers;
-        const intern = new Intern (name, id, email, school);
-        staff.push(internAnswers);
-        console.log(intern);
-        });
+const addIntern = async () => {
+    const interAnswers = await inquirer.prompt(internQuest);
+    const { name, id, email, school } = internAnswers;
+    const intern = new Intern(name, id, email, school);
+    staff.push(internAnswers);
+    console.log(intern);
+    addStaff();
 };
 
 // Function to create HTML page
@@ -100,7 +98,7 @@ const writeFile = data => {
 };
 
 //Function to write Manager and Staff to HTML page
-addManager ()
+addManager()
 .then(addStaff)
 .then(staff => {
     return generateHTML(staff);
