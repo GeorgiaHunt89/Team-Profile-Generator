a67a2cbc8ef73ed4c7dea40b82d9876f46de45d0
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-08 12:39:16 +0930

    Cleaned up code for prompts and models

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
-const employeeQuest = require('./quest/Employee.quest');
 const managerQuest = require('./quest/Manager.quest');
-const engineerQuest = require('./quest/Engineer.quest');
-const internQuest = require('./quest/Intern.quest');
 
-//Constructors
-const Employee = require('./lib/Employee');
+//Prompts
+const addEmployee = require('./lib/Prompt/addEmployee');
+const addEngineer = require('./lib/Prompt/addEngineer');
+const addIntern = require('./lib/Prompt/addIntern');
+// models
 const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
 const generateHTML = require('./src/generateHTML');
+const { resolve } = require('path');
 
 // Empty array to hold user responses
 const userResponse = [];
@@ -21,7 +20,7 @@ const addManager = async () => {
     const managerAnswers = await inquirer.prompt(managerQuest)
     const {name, id, email, officeNumber} = managerAnswers;
     const manager = new Manager (name, id, email, officeNumber);
-    userResponse.push(managerAnswers);
+    userResponse.push(manager);
     console.log(manager)
 };
 
@@ -32,79 +31,51 @@ const addStaff = async () => {
 
    const userInput = await inquirer.prompt([
         {
-            type: 'checkbox',
+            type: 'list',
             name: 'staffSelection',
             message: 'Which staff member would you like to add next?',
             choices: [
-                'Employee', 'Intern', 'Engineer',
+                'Employee', 'Intern', 'Engineer', 'No more'
             ],
         },
     ]);
     switch (userInput.staffSelection) {
         case 'Engineer':
-            addEngineer();
+            const engineer = await addEngineer();
+            userResponse.push(engineer);
             break;
         case 'Intern':
-            addIntern();
+            const intern =  await addIntern();
+            userResponse.push(intern);
             break;
         case 'Employee':
-            addEmployee();
+            const employee = await addEmployee();
+            userResponse.push(employee);
             break;
         default:
-            generateHTML();
+            const HTMLFile = await generateHTML(userResponse)
+            console.log(HTMLFile);
+            await writePage(HTMLFile);
+            process.exit(0);
     };
+
     console.log('Add:', userInput.staffSelection);
-};
-
-//Function to record Employee answer from user
-const addEmployee = async () => {
-    const employeeAnswers = await inquirer.prompt(employeeQuest);
-    const {name, id, email} = employeeAnswers;
-    const employee = new Employee(name, id, email);
-    userResponse.push(employeeAnswers);
-    console.log(employee);
     addStaff();
 };
-
-// Function to record Engineer answers from user
-const addEngineer = async () => {
-    const engineerAnswers = await inquirer.prompt(engineerQuest);
-    const {name, id, email, gitHub} = engineerAnswers;
-    const engineer = new Engineer(name, id, email, gitHub);
-    userResponse.push(engineerAnswers);
-    console.log(engineer);
-    addStaff();
-};
-
-//Function to record Intern answer from user
-const addIntern = async () => {
-    const internAnswers = await inquirer.prompt(internQuest);
-    const {name, id, email, school} = internAnswers;
-    const intern = new Intern(name, id, email, school);
-    userResponse.push(internAnswers);
-    console.log(intern);
-    addStaff();
-};
-
 
 // Function to create HTML page
-const writePage = userResponse => {
-    fs.writeFile('./dist/index.html', userResponse, err => {
+const writePage = HTML => new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', HTML, err => {
         if (err){
-            console.log (err);
-            return;
+            reject(err);
         } else {
+            resolve();
             console.log('You have successfully created your team profile')
         }
     })
-};
+});
 
 console.log(`Welcome to the Team Profile Generator, let's start adding your employees!`);
 
 addManager()
-.then(addStaff)
-.then(userResponse => generateHTML(userResponse))
-.then(HTMLFile => writePage(HTMLFile))
-.catch(err => {
-    console.log(err);
-});
+.then(addStaff);
